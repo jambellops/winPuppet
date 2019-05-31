20dbe4e5c89c35e0e7cad85b515a305c08daf318
jambellops <bellagioj@gmail.com>
2019-05-31 10:17:52 -0700

    sanitize windows manifest

--- site/profile/manifests/windows/wsus_client.pp
+++ site/profile/manifests/windows/wsus_client.pp
@@ -1,7 +1,7 @@
 # Profile for windows wsus_client
 class profile::windows::wsus_client {
   case $::hostname {
-    /-(orl|tm1)|(blr1|del1)-dc/:  {
+    /-(mytag1|mytag2)|(intl2|intl3)-dc/:  {
       $auto_update_option           = lookup('wsus_client::altcase::auto_update_option')
       $scheduled_install_day        = undef
       $scheduled_install_hour       = undef
@@ -15,15 +15,15 @@ class profile::windows::wsus_client {
     }
   }
   $target_group = $::hostname ? {
-    /-orl/                                     => lookup('wsus_client::oracle::target_group'),
-    /-tm1/                                     => lookup('wsus_client::tm1::target_group'),
-    /(?!irn1-dc\d)irn1|(?!acm1-dc\d)acm1|yyz1/ => lookup('wsus::atg_servers::target_group'),
+    /mytag1/                                   => lookup('wsus_client::oracle::target_group'),
+    /mytag2/                                   => lookup('wsus_client::tm1::target_group'),
+    /(?!loc1-dc\d)loc1|(?!loc2-dc\d)loc2|intl1/=> lookup('wsus::exception_servers::target_group'),
     /-dc\d/                                    => lookup('wsus_client::domain_controllers::target_group'),
     default                                    => lookup('wsus_client::default::target_group'),
   }
 
   class { 'wsus_client':
-    server_url                     => "http://sjc1-wsus01.${::domain}:8530",
+    server_url                     => "http://my-wsus01.${::domain}:8530",
     auto_update_option             => $auto_update_option,
     scheduled_install_day          => $scheduled_install_day,
     scheduled_install_hour         => $scheduled_install_hour,
